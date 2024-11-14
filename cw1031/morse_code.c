4b3449619317fc4ac506e7e4b65a7e6c7a682a59
Cristina PC <cg01237@surrey.ac.uk>
2024-11-14 17:04:25 +0000

    fixing syntax

--- cw1031/morse_code.c
+++ cw1031/morse_code.c
@@ -19,7 +19,7 @@
 // declare global variables 
 uint32_t start_time, end_time, timePressed, pause_start, pause_duration;
 char morse_input[4] = {0};
-int index = 0;
+int morse_input_index = 0;
 
 // Function prototypes
 void checkButton();
@@ -72,11 +72,11 @@ int main() {
 		if (pause_duration > INTERLETTER){ //ADD LOGIC FOR: AND IF mourse_input size reached 4!!
 			decoder(morse_input);
 			memset(morse_input, 0, sizeof(morse_input)); //based on https://www.geeksforgeeks.org/memset-c-example/
-			index = 0; 
+			morse_input_index = 0; 
 		} else {
 			checkButton();
 		}
-   		
+   }
 }
 
 
@@ -92,10 +92,10 @@ void decoder(const char *input){
 
 void checkButton() {
 	if (timePressed < DOT_THRESHOLD) { 
-		morse_input[index++]= '.';
+		morse_input[morse_input_index++]= '.';
 		//printf(".");  
 	} else if (timePressed > DOT_THRESHOLD) { 
-		morse_input[index++] = '-';
+		morse_input[morse_input_index++] = '-';
 	}
 }
 
