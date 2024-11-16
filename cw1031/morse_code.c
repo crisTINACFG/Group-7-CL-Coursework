71b1f8d8b01fbcb85ee4376c117b50eef3656175
Karous <mk02523@heron112.eps.surrey.ac.uk>
2024-11-16 17:43:40 +0000

    buzzer sucks

--- cw1031/morse_code.c
+++ cw1031/morse_code.c
@@ -4,20 +4,53 @@
 #include "includes/seven_segment.h"
 #include "hardware/gpio.h"
 #include <time.h>
+#include "hardware/pwm.h"
+#include "includes/buzzer.h"
 
 #define DOT_THRESHOLD 250      // Less than 250ms = dot, more than 250ms = dash
 #define INTERLETTER 400        // More than 400ms = new letter
 #define BUTTON_PIN 16          // GPIO pin for button
-
+#define DEBOUNCE_DELAY 200  ///to ensure clean button press duration
 // Declare global variables
 uint32_t start_time, end_time, timePressed, pause_start, pause_duration;
 char morse_input[5];           // Store up to 4 symbols + null terminator
 int morse_input_index = 0;
+int frequency = 500;
 
 // Function prototypes
 void checkButton();
 void decoder(const char *input);
 void Letter();
+void welcome_song();
+
+void playNote( int frequency){
+    buzzer_enable(frequency);
+}
+void welcome_song() {
+    unsigned int song[] = {C,C,G,C,G};
+    unsigned int songLength = sizeof(song)/sizeof(song[0]);
+
+    for (unsigned int i = 0; i < songLength; i++){
+        buzzer_enable(song[i]);
+        sleep_ms(100);
+        buzzer_disable();
+        sleep_ms(100);
+    }
+    //buzzer_disable();
+}
+void errorSong(){
+    unsigned int song[] = {A4,B4,A4,B4,A4,B4,E3};
+    unsigned int songLength = sizeof(song)/sizeof(song[0]);
+
+    for (unsigned int i = 0; i < songLength; i++){
+        buzzer_enable(song[i]);
+        sleep_ms(100);
+        buzzer_disable();
+        sleep_ms(50);
+    }
+    buzzer_disable();
+
+}
 
 // Morse code dictionary
 const char morse_code[26][5] = {
@@ -53,20 +86,17 @@ uint32_t time_ms() {
     return to_ms_since_boot(get_absolute_time());
 }
 
-int main() {
+int main() { 
     timer_hw->dbgpause = 0;
     stdio_init_all();
+    buzzer_init();
+    //buzzer_disable();
+    //welcome_song();
 
     printf("Welcome\n");
     seven_segment_init();
     sleep_ms(1000);
     seven_segment_off();
-	// if(true) {
-	// 	seven_segment_show(5);
-	// 	sleep_ms(1000);
-	// 	seven_segment_off();
-	// 	sleep_ms(100);
-	// }
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
     gpio_pull_down(BUTTON_PIN);
@@ -79,7 +109,9 @@ int main() {
 }
 
 void checkButton() {
+    //buzzer_disable();
     while (!gpio_get(BUTTON_PIN)) {
+        buzzer_disable();
         // Button not pressed, check for inter-letter pause
         if (pause_start > 0) {
             pause_duration = time_ms() - pause_start;
@@ -93,13 +125,16 @@ void checkButton() {
     // Button pressed
     start_time = time_ms();
     while (gpio_get(BUTTON_PIN)) {
+        buzzer_enable(frequency);
         sleep_ms(20);
     }
+    //buzzer_disable();
 
     // Button released
     end_time = time_ms();
     timePressed = end_time - start_time;
     pause_start = time_ms(); // Mark pause start
+    //buzzer_disable();
 
     if (timePressed < DOT_THRESHOLD) {
         if (morse_input_index < 4) {
@@ -107,6 +142,7 @@ void checkButton() {
             morse_input_index++;
         } else {
             printf("Error: Input exceeds limits.\n");
+            errorSong();
         }
     } else {
         if (morse_input_index < 4) {
@@ -114,6 +150,8 @@ void checkButton() {
             morse_input_index++;
         } else {
             printf("Error: Input exceeds limits.\n");
+            errorSong();
+
         }
     }
 
@@ -133,9 +171,9 @@ void decoder(const char *input) {
         if (strcmp(input, morse_code[i]) == 0) {
             seven_segment_init();
             seven_segment_show(i);
-            sleep_ms(500);
+            sleep_ms(1000);
             seven_segment_off();
-            sleep_ms(500);
+            //sleep_ms(1000);
             printf("Letter detected: %c\n", 'A' + i);
             
             return;
@@ -143,7 +181,10 @@ void decoder(const char *input) {
     }
 
     printf("Error: This morse code does not exist.\n");
+    errorSong();
+
     seven_segment_off(); // Turn off display for errors
 }
+
 
 
